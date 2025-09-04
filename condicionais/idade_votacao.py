174fa77521683a3bbcfc2cb699f173ee99e2650b
antonyy22 <puginskiantony@gmail.com>
2025-09-03 21:49:29 -0300

    inicio de conteudo de condicionais

---
+++ condicionais/idade_votacao.py
@@ -0,0 +1,5 @@
+idade= int (input("Informe sua idade -> "))
+if idade >=16:
+    print ("Pode votar !")
+else:
+    print("Não pode votar")
---
+++ condicionais/verificar-positivo.py
@@ -0,0 +1,8 @@
+numero= int (input("Informe um numero -> "))
+if numero >0:
+    print ("O numero é positivo! ")
+elif numero < 0:
+    
+    print ("O numero é negativo ") 
+else:
+    print ("O numero é zero ")
